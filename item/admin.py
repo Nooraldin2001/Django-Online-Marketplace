31d0acd9d6efc8627c4d55dfb92ff1837ca55029
Nooraldin2001 <noorabdalkadir@std.mans.edu.eg>
2023-11-11 11:02:29 +0200

    adding Item models

--- item/admin.py
+++ item/admin.py
@@ -1,3 +1,9 @@
+from atexit import register
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Category, Item
+
+admin.site.register(Category)
+admin.site.register(Item)
