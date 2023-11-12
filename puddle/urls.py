9fd17d8825e2bafe6a8fa34422ba3fd71393c71f
Nooraldin2001 <noorabdalkadir@std.mans.edu.eg>
2023-11-12 13:30:02 +0200

    cleaning up the urls

--- puddle/urls.py
+++ puddle/urls.py
@@ -22,11 +22,10 @@ from django.contrib import admin
 from django.urls import include, path
 
 #views imports
-from core.views import index, contact
+
 
 urlpatterns = [
-    path('', index, name='index'),
+    path('', include('core.urls')),
     path('items/', include('item.urls')),
-    path('contact/', contact, name='contact'),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
